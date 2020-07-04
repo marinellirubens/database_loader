18018e85cd2ad2ad5a353f218787242b06d95f93
marinellirubens <marinelli.rubens@gmail.com>
2020-07-03 23:04:45 -0300

    Change on version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='database_loader',
-    version='1.0.9',
+    version='1.0.10',
     packages=['database_loader', 'database_loader.core'],
     url='',
     license='',
