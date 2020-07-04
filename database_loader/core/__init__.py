2a782269ccc00f7cd2f417475e3b8dfe16825e6e
marinellirubens <marinelli.rubens@gmail.com>
2020-07-04 02:52:57 -0300

    Updated version

--- database_loader/core/__init__.py
+++ database_loader/core/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 from database_loader.core import loader
+from database_loader.core import databases
---
+++ database_loader/core/databases/__init__.py
@@ -0,0 +1,3 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+from database_loader.core.databases import builder, database
